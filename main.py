19568a4682598d5bfdb811589201027d19e04ea9
tayl0rswiftfan1 <spencermdeng@gmail.com>
2025-12-08 17:07:15 -0500

    initial commit

--- main.py
+++ main.py
@@ -3,68 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 '''
-#loading the image
-image = cv2.imread("JRC.png") #destination of the photo or whatever
-
-#gay the image
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imwrite("gray.png", gray)
-
-#guassian blur the photo ... (input array, kernel size, and how much blur you can get)
-blur = cv2.GaussianBlur(gray, (7,7), 10)
-cv2.imwrite("blur.png", blur)
-
-#thresholding to crate a binary mask
-thresh = cv2.threshold(gray, 0, 255,  cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-cv2.imwrite("uhh.png", thresh)
+Name: Spencer
+Date: 12/8/25
+About: colour threshold an image and create bounding box for the object. This can be used in order to 
 
 
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
-cv2.imwrite("kern.png", kernel)
-
-dilate = cv2.dilate(thresh, kernel, iterations=1)
-cv2.imwrite("dilate.png", dilate)
-
-# create binary mask, only external contours,  compress points
-contours, heiarchy = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-#draw the contours
-imageContors = cv2.drawContours(image, contours, -1, (0, 255, 0), 2)  # green contours
-cv2.imwrite("contours.png", imageContors)
 
 '''
 
-'''
-#thresholding check
-
-def thresholding(imgpath):
-
-#whats the image!
-    image = cv2.imread(imgpath)
-    #image to grayscale (skip this instead use HSV)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    threshOptions = [cv2.THRESH_BINARY, cv2.THRESH_BINARY_INV, cv2.THRESH_TOZERO, cv2.THRESH_TRUNC]
-    titles = ["BINARY", "BINARY_INV", "TOZERO", "TRUNC"]
-
-    plt.figure(figsize=(8, 8))
-
-#show the threshold
-
-    for i in range(len(threshOptions)):
-        retVal, thresh = cv2.threshold(gray, 185, 255, threshOptions[i])
-
-        plt.subplot(2, 2, i+1)
-        plt.imshow(thresh, cmap='gray')
-        plt.title(titles[i])
-        plt.axis("off")
-
-    plt.tight_layout()
-    plt.show()
-
-thresholding("JRC.png")
-
-'''
 
 def colourThreshold (imgpath):
 
