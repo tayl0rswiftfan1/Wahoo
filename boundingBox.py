e236f7b1ad1adb3b3ad78b89b58330192b2c1011
tayl0rswiftfan1 <spencermdeng@gmail.com>
2026-01-08 14:25:56 -0500

    ConvType2  (nrrd --> png)

--- boundingBox.py
+++ boundingBox.py
@@ -1,6 +1,7 @@
 '''
 Name: Spencer
 Date: 12/8/25
+Last Modified: 1/8/26
 About: colour threshold an image and create bounding box for the object. 
 This can be used in order to do something useful 
 
@@ -12,10 +13,13 @@ import os
 import platform #check the os of the user
 import matplotlib.pyplot as plt
 
-#to convert dcm -> jpg
+#to convert dcm -> png
 import pydicom
 from PIL import Image
 
+#to convert nrrd -> png
+import SimpleITK as itk
+from PIL.ImageColor import colormap
 
 '''
 Convert the DCM image file into a PNG (Lossless Compression)
@@ -40,10 +44,62 @@ def convType (file):
     final.show()
 
     #save as png
-    final.save(file + ".png")
+    final.save(file + ".png") #save in folder
+    #print (file + ".png")
 
     #return filename.png
     return file + ".png"
+
+
+'''
+Convert Nrrd file into PNG, this takes both the segmentation mask and the medical image (CT) and converts it into a PNG 
+@param medImg (str): nrrd file path of the medical image 
+@param segImg (str): nrrd file paht of the segmentaion mask 
+'''
+
+def convType2 (medImg, segImg):
+
+    #reading the medical image and the segmenation
+    medImage = itk.ReadImage(medImg)
+    segImage = itk.ReadImage(segImg)
+
+    #use numpy to get the array of images and seg.. output shape is [z,y,x]
+    medImageArray = itk.GetArrayFromImage(medImage)
+    segImageArray = itk.GetArrayFromImage(segImage)
+
+    #line up the slice with the seg
+    slice =  medImageArray.shape[0] // 2 #set arb middle slice
+
+    #this can be looped for all slices
+
+    #line up slices (@ selected slice)
+    imgSlice = medImageArray [slice]
+    segSlice = segImageArray [slice]
+
+    '''
+    #slice normalization .. idk
+    imgSliceNormd = cv2.normalize(imgSlice, None, 0, 255, cv2.NORM_MINMAX)
+    imgSliceNormd = imgSlice.astype(np.uint8)
+    '''
+    #manually normalize CT image slice
+    imgSliceNormd = np.clip(imgSlice, -1000, 3000)
+
+    #add 1000 to the HU to get rid of the negative pixels, *255 scales to 8-bit img
+    imgSliceNormd = ((imgSliceNormd + 1000)/ 4000 * 255).astype(np.uint8)
+
+    #overlay the mask @ slice.. binary mask
+    overlayMask = (segSlice == 1)
+
+    #convert the ct to BGR
+    colourCT = cv2.cvtColor(imgSliceNormd, cv2.COLOR_GRAY2BGR)
+
+    #put the mask on
+    colourCT [overlayMask] = [0,255,0]
+
+    #save the file
+    cv2.imwrite("seg+CT.png", colourCT)
+
+
 
 
 
